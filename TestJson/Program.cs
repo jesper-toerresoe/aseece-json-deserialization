d119ce4890dec8a11582d159f95f4d97749c9a1b
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2018-11-08 14:41:17 +0100

    Add project files.

---
+++ TestJson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
